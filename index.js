e8941eedf3baf197a5e009c457bae6fca75d3c2b
rafidianto <rafidianto.code@gmail.com>
2021-06-26 20:25:42 +0700

    bug fixing : add port, and properly get the json data

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.post('/', async (req, res) => {
-  res.json(res.data);
+  let data = req.body;
+  console.log(data);
+  res.json(data);
 });
 
-app.listen();
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to webhook !',
+  });
+});
+
+app.listen(5000, () => {
+  console.log('start server on port 5000');
+});
